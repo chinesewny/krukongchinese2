da7d7aeb991d5aa7b661f4dff87f17032c659a6f
chinesewny <chinesewny@gmail.com>
2026-01-15 10:04:32 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -171,5 +171,3 @@ async function saveAndRefresh(payload) {
     // 1. If it's a Login request, send immediately (no queue)
     if(payload.action === 'login') {
         showLoading("กำลังตรวจสอบ...");
-    }
-}
